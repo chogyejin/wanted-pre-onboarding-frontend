ef161076d756c07509ed6b71e824345984d8d064
chogyejin <whrpwls96@naver.com>
2022-10-17 15:11:19 +0900

    fix: Fix axios instance default header

--- src/lib/axios/index.ts
+++ src/lib/axios/index.ts
@@ -1,12 +1,12 @@
+import { getToken } from "./../localStorage/index";
 import axios from "axios";
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  timeout: 1000,
 });
 
-const AUTH_TOKEN = localStorage.getItem("auth-token");
+const AUTH_TOKEN = getToken();
 
-instance.defaults.headers.common["Authorization"] = AUTH_TOKEN;
+instance.defaults.headers.common["Authorization"] = `Bearer ${AUTH_TOKEN}`;
 
 export default instance;
